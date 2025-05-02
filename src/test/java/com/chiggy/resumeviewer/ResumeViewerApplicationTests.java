de3044abab17e3802b994b5485a29bd43075db06
Chiggy-Playz <garg.chirag2004@gmail.com>
2025-05-03 01:33:23 +0530

    Init

---
+++ src/test/java/com/chiggy/resumeviewer/ResumeViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chiggy.resumeviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
